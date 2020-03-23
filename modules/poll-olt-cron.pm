b08dd3a512b10a98799f0ff16df95e3bacd65b84
Nikita Proschin <38611552+Nekut-02@users.noreply.github.com>
2020-03-23 13:55:53 +0300

    Update poll-olt-cron.pm

--- modules/poll-olt-cron.pm
+++ modules/poll-olt-cron.pm
@@ -1,118 +1,81 @@
 #!/usr/bin/perl
+use strict;
+use utf8;
 use warnings;
 use strict;
+use Socket;
 use DBI;
-use utf8;
 
-binmode(STDOUT,':utf8');
+use Data::Dumper;
+binmode(STDOUT, ':utf8');
 
 my %OID = (
-    mac     => '1.3.6.1.4.1.17409.2.3.4.1.1.7',
-    signal  => '1.3.6.1.4.1.17409.2.3.4.2.1.4'
+    mac    => '1.3.6.1.4.1.17409.2.3.4.1.1.7',
+    signal => '1.3.6.1.4.1.17409.2.3.4.2.1.4'
 );
 
 my $source   = "DBI:mysql:cdata:localhost";
 my $username = "cdata";
 my $password = "cdata";
-my $dbh = DBI->connect($source, $username, $password, {mysql_enable_utf8 => 1});
+my $dbh      = DBI->connect(
+    $source,
+    $username,
+    $password, {
+        mysql_enable_utf8       => 1,
+        PrintError              => 1,
+        mysql_client_found_rows => 1,
+    }
+);
+
 $dbh->do("set names utf8");
 
-my $sth = $dbh->prepare("SELECT ip FROM olt;");
-$sth->execute;
+my $ips = $dbh->selectcol_arrayref("select ip from olt");
 
-while (my $ref = $sth->fetchrow_hashref()) {
+for my $ip_address (@$ips) {
 
-    my $ip_address = $ref->{'ip'};
+    my $ip_address_conver = unpack "N", inet_aton($ip_address);
 
     my $snmp_mac    = `snmpwalk -v2c -c public $ip_address $OID{mac}`;
     my $snmp_signal = `snmpwalk -v2c -c public $ip_address $OID{signal}`;
-   
-    my @mac     = split(/\n/, $snmp_mac);
-    my @signal  = split(/\n/, $snmp_signal);   
 
-    for(@mac) { 
-        if( /\.(\d+)\s+=\s+Hex-STRING:\s+([0-9A-F\s]+)/ ) { 
-            my ($port, $mac) = ($1, $2); 
-            $mac =~ s/\s+$//; 
-            $mac =~ tr/A-Z/a-z/;
-            $mac =~ tr/ /:/; 
-            comparison(
-                mac_port    => $port,
-                mac         => $mac,
-                ip_olt      => $ip_address
-
-            );
-            add(
-                mac_port    => $port,
-                mac         => $mac,
-                ip_olt      => $ip_address
-            );
-        } 
+    my %ports =
+      map {
+        my ($port, $mac);
+        (($port, $mac) = /\.(\d+)\s+=\s+Hex-STRING:\s+([0-9A-F\s]+)/)
+          ? ($port, {mac => $mac =~ s/\s+$//r =~ tr/ /:/r =~ tr/A-Z/a-z/r})
+          : ()
+      }
+      split(/\n/, $snmp_mac);
+    for (split(/\n/, $snmp_signal)) {
+        if (/\.(\d+).0.0\s+=\s+INTEGER:\s+(-\d+)/) {
+            $ports{$1}{signal} = sprintf("%.1f", $2 / 100);
+        }
     }
-
-    for (@signal){
-        if ( /\.(\d+).0.0\s+=\s+INTEGER:\s+(-\d+)/ ){
-            my ($port, $signal) = ($1, $2);
-            my @a = map { $_ / 100} $signal;  
-            my $signal_tenth = sprintf("%.1f", @a);
-            comparison(
-                signal_port => $port,
-                signal      => $signal_tenth,
-                ip_olt      => $ip_address
-            );
-            add(
-                signal_port => $port,
-                signal      => $signal_tenth,
-                p_olt       => $ip_address
-            );              
+    for my $port (keys %ports) {
+        if (exists $ports{$port}{signal}) {
+            my $updated =
+              $dbh->do("UPDATE olt_$ip_address_conver SET sugnal=? WHERE number=?", undef, $ports{$port}{signal}, $port);
+            if ($updated == 0) {
+                $dbh->do(
+                    "INSERT INTO olt_$ip_address_conver (number, sugnal, mac, address, serial) VALUES(?, ?, ?, '','')",
+                    undef, $port,
+                    $ports{$port}{signal},
+                    $ports{$port}{mac}
+                );
+            }
+        } else {
+            my ($olt_count) =
+              $dbh->selectrow_array("SELECT Count(number) FROM olt_$ip_address_conver WHERE number = ?",
+                undef, $port);
+            if (!$olt_count) {
+                $dbh->do(
+                    "INSERT INTO olt_$ip_address_conver (number, sugnal, mac, address, serial) VALUES(?, ?, ?, '','')",
+                    undef, $port, undef, $ports{$port}{mac});
+            }
         }
     }
 }
 
-sub comparison {  
-    my (%h)         = @_;
-    my $ip          = delete $h{ip_olt};
-    my $mac_port    = delete $h{mac_port};
-    my $mac         = delete $h{mac};
-    my $signal_port = delete $h{signal_port};
-    my $signal      = delete $h{signal};
-    if ( $mac_port = $signal_port ) {
-        refresh(
-            port    => $mac_port,
-            signal  => $signal,
-            ip_olt  => $ip
-        );
-    }
-}
-
-sub refresh {
-    my (%h)     = @_;
-    my $ip      = $h{ip_olt};
-    my $port    = $h{port};
-    my $signal  = $h{signal};
-    my $olt_count = $dbh->selectrow_array("SELECT Count(number) FROM olt_$ip WHERE number = ?", undef,  $port) ;
-    if ($olt_count > 0) {
-        $dbh->do("UPDATE olt_$ip SET sugnal=? WHERE number=?", undef, $signal, $port) ;
-        print "update $port\n";
-    }
-}
-
-sub add {
-    my (%h) = @_;
-    my $ip          = $h{ip_olt};
-    my $mac_port    = $h{mac_port};
-    my $mac         = $h{mac};
-    my $signal_port = $h{signal_port};
-    my $signal      = $h{signal};
-
-    my $olt_count =
-      $dbh->selectrow_array("SELECT Count(number) FROM olt_$ip WHERE number = ?", undef, $mac_port);
-    unless ($olt_count > 0) {
-        $dbh->do("INSERT INTO olt_$ip VALUES(?, ?, ?, '','','');", undef, $mac_port, $signal, $mac);
-    }
-}
-
-$sth->finish;    
 $dbh->disconnect;
 
 1;
